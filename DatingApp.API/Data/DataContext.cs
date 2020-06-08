d24a2a22823bfcc49d4a1320cc6eeca2846a4443
tofikkhan.pathan1@outlook.com <tofikkhan.pathan1@outlook.com>
2020-06-08 14:28:45 +0530

    GIT Checkin test

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        // constructor for data context
+        public  DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; } // Values = Table name created on SQLite
+
+
+    }
+}
